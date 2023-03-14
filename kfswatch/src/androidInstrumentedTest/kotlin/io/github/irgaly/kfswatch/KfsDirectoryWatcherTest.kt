6d3b817bf6dddc22eee802d4cf9f7c21f79fb726
irgaly <irgaly@gmail.com>
2023-03-15 01:32:23 +0900

    add simple KfsDirectoryWatcher test

--- kfswatch/src/androidInstrumentedTest/kotlin/io/github/irgaly/kfswatch/KfsDirectoryWatcherTest.kt
+++ kfswatch/src/androidInstrumentedTest/kotlin/io/github/irgaly/kfswatch/KfsDirectoryWatcherTest.kt
@@ -2,15 +2,13 @@ package io.github.irgaly.kfswatch
 
 import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import io.github.irgaly.kfswatch.internal.platform.FileWatcher
-import io.github.irgaly.kfswatch.internal.platform.Logger
+import app.cash.turbine.test
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
@@ -18,6 +16,16 @@ class KfsDirectoryWatcherTest {
     companion object {
         private val context get() = getInstrumentation().targetContext
         private val testDirectory get() = context.filesDir.resolve("test")
+        private val logger
+            get() = object : KfsLogger {
+                override suspend fun debug(message: String) {
+                    Log.d("KfsLogger", message)
+                }
+
+                override suspend fun error(message: String) {
+                    Log.e("KfsLogger", message)
+                }
+            }
 
         @BeforeAll
         @JvmStatic
@@ -38,35 +46,19 @@ class KfsDirectoryWatcherTest {
         val dir = testDirectory.resolve("test1").also {
             it.mkdir()
         }
-        val watcher = FileWatcher(
-            onEvent = { target, path, event ->
-                Log.d("event", "event: $target/$path: $event")
-            },
-            onStart = {
-                Log.d("onStart", "onStart: $it")
-            },
-            onStop = {
-                Log.d("onStop", "onStop: $it")
-            },
-            onError = { target, message ->
-                Log.e("error", "error: $target : $message")
-            },
-            logger = object : Logger {
-                override fun debug(message: () -> String) {
-                    Log.d("logger", message())
-                }
-
-                override fun error(message: () -> String) {
-                    Log.e("logger", message())
-                }
-            }
+        val watcher = KfsDirectoryWatcher(
+            this,
+            logger = logger
         )
-        watcher.start(listOf(dir.path))
-        delay(100.milliseconds)
-        dir.resolve("child1").mkdir()
-        delay(100.milliseconds)
+        watcher.onEventFlow.test {
+            watcher.add(dir.path)
+            dir.resolve("child1").mkdir()
+            awaitItem() should {
+                it.event shouldBe KfsEvent.Create
+                it.path shouldBe "child1"
+            }
+        }
         watcher.close()
-        delay(100.milliseconds)
     }
 
     @Test
