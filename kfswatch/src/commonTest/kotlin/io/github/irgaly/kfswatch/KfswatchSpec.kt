7a617ff889fd68aecc29e328e7ee37d060779699
irgaly <irgaly@gmail.com>
2023-03-18 14:47:56 +0900

    test: add awaitEvents()

--- kfswatch/src/commonTest/kotlin/io/github/irgaly/kfswatch/KfswatchSpec.kt
+++ kfswatch/src/commonTest/kotlin/io/github/irgaly/kfswatch/KfswatchSpec.kt
@@ -6,6 +6,7 @@ import app.cash.turbine.testIn
 import io.github.irgaly.test.DescribeFunSpec
 import io.github.irgaly.test.extension.tempdir
 import io.github.irgaly.test.platform.Files
+import io.kotest.assertions.fail
 import io.kotest.core.test.TestScope
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
@@ -51,6 +52,23 @@ class KfswatchSpec : DescribeFunSpec({
         }
     }
 
+    suspend fun ReceiveTurbine<KfsDirectoryWatcherEvent>.awaitEvents(vararg events: Event) {
+        val list = events.toMutableList()
+        (0..list.lastIndex).forEach {
+            val item = awaitItem()
+            val index = list.indexOfFirst { event ->
+                (event.event == item.event) &&
+                        (event.path == item.path) &&
+                        (event.targetDirectory?.let { it == item.targetDirectory } ?: true)
+            }
+            if (index < 0) {
+                fail("$item is not expected in: ${events.joinToString(",")}")
+            } else {
+                list.removeAt(index)
+            }
+        }
+    }
+
     describe("基本機能") {
         it("directory, file の Create, Delete, Rename(Delete -> Create) を検出できる") {
             val directory = "$tempDirectory/simple".also { mkdirs(it) }
@@ -61,13 +79,17 @@ class KfswatchSpec : DescribeFunSpec({
                 mkdirs("$directory/child1")
                 awaitEvent(KfsEvent.Create, "child1")
                 Files.writeFile("$directory/child2", "test")
-                awaitEvent(KfsEvent.Create, "child2")
-                awaitEvent(KfsEvent.Modify, "child2")
+                awaitEvents(
+                    Event(KfsEvent.Create, "child2"),
+                    Event(KfsEvent.Modify, "child2")
+                )
                 Files.writeFile("$directory/child3", "")
                 awaitEvent(KfsEvent.Create, "child3")
                 Files.move("$directory/child2", "$directory/child3")
-                awaitEvent(KfsEvent.Delete, "child2")
-                awaitEvent(KfsEvent.Create, "child3")
+                awaitEvents(
+                    Event(KfsEvent.Delete, "child2"),
+                    Event(KfsEvent.Create, "child3")
+                )
             }
             errors.ensureAllEventsConsumed()
             errors.cancel()
@@ -123,9 +145,25 @@ class KfswatchSpec : DescribeFunSpec({
             errors.cancel()
             watcher.close()
         }
+        it("監視対象の親ディレクトリの移動は検出しない") {
+            val directory = "$tempDirectory/target_parent".also { mkdirs(it) }
+            val parent = "$directory/parent".also { mkdirs(it) }
+            val target = "$parent/target".also { mkdirs(it) }
+            val watcher = createWatcher()
+            val errors = watcher.onErrorFlow.testIn(this)
+            watcher.onEventFlow.test {
+                watcher.add(target)
+                Files.move(parent, "$directory/parent2")
+                Files.writeFile("$directory/parent2/target/child", "")
+                awaitEvent(KfsEvent.Create, "child")
+            }
+            errors.ensureAllEventsConsumed()
+            errors.cancel()
+            watcher.close()
+        }
         it("子ディレクトリ内の変化は検出しない") {
             val directory = "$tempDirectory/directory_child".also { mkdirs(it) }
-            val child = "$directory/child"
+            val child = "$directory/child".also { mkdirs(it) }
             val watcher = createWatcher()
             val errors = watcher.onErrorFlow.testIn(this)
             watcher.onEventFlow.test {
@@ -138,3 +176,9 @@ class KfswatchSpec : DescribeFunSpec({
         }
     }
 })
+
+private data class Event(
+    val event: KfsEvent,
+    val path: String,
+    val targetDirectory: String? = null
+)
