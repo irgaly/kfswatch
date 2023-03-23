dfd1c5c724d5c59b0e560a57c888ea9534986ede
irgaly <irgaly@gmail.com>
2023-03-24 00:12:13 +0900

    Add :sample:android

---
+++ sample/android/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.buildlogic.android.application)
+}
+
+android {
+    namespace = "io.github.irgaly.kfswatch.sample"
+    defaultConfig {
+        applicationId = "io.github.irgaly.kfswatch.sample"
+        versionName = libs.versions.kfswatch.get()
+        minSdk = 21
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+dependencies {
+    implementation(dependencies.platform(libs.compose.bom))
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.bundles.compose)
+    implementation(libs.kfswatch)
+}
